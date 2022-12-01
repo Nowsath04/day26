7b7578c314e5f98abe4ac84eee5e40403abc7bce
Nowsath04 <110928124+Nowsath04@users.noreply.github.com>
2022-12-01 11:55:39 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./css/fontawesome-free/css/all.min.css";
+import "./css/sb-admin-2.css";
+import Dashboard from "./Dashboard";
+import User from "./User";
+import Topbar from "./Topbar";
+import Sidebar from "./Sidebar";
+import Createuser from "./Createuser";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ViewUser from "./ViewUser";
+import Edituser from "./Edituser";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div id="wrapper">
+        <Sidebar />
+        <div id="content-wrapper" className="d-flex flex-column">
+          <div id="content">
+            <Topbar />
+            <Routes>
+              <Route path="/dashboard" element={<Dashboard />}></Route>
+              <Route path="/users" element={<User />}></Route>
+              <Route path="/user_create" element={<Createuser />}></Route>
+              <Route path="/user_create/:id" element={<Createuser />}></Route>
+              <Route path="/view/:id" element={<ViewUser />}></Route>
+              <Route path="/edit/:id" element={<Edituser />}></Route>
+              
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
